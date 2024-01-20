6f347eac56ff290b0bd19fcf75b961516bb65bd3
AzarAhmadov <ehmedovh111@gmail.com>
2024-01-21 00:23:24 +0400

    Contact page finished

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,26 @@
+import Image from 'next/image'
 import React from 'react'
 
 const page: React.FC = () => {
     return (
-        <div>
-            Contact
-        </div>
+        <main>
+            <section id='contact'>
+                <div className="row">
+                    <div className="left">
+                        <Image fill src='/images/contact.png' alt='Contact' />
+                    </div>
+                    <div className="right">
+                        <form action="">
+                            <input type="text" placeholder='Name and Surname' />
+                            <input type="email" placeholder='Email Adress' />
+                            <input type="text" placeholder='Phone number (Optional)' />
+                            <textarea placeholder='Message'></textarea>
+                            <button type='submit'> Send </button>
+                        </form>
+                    </div>
+                </div>
+            </section>
+        </main>
     )
 }
 
