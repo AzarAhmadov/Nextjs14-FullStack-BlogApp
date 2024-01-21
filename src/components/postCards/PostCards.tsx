eeaab2e713d4264391448d8849e20aab27dbc621
AzarAhmadov <ehmedovh111@gmail.com>
2024-01-21 10:28:05 +0400

    Post Cards component finished

---
+++ src/components/postCards/PostCards.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import { FaLongArrowAltRight } from "react-icons/fa";
+import { CgCalendarDates } from "react-icons/cg";
+
+
+const PostCards = () => {
+    return (
+        <section className='post-cards'>
+            <div className="post-content">
+                <div className='image-container'>
+                    <Image fill src='/images/post.png' alt='post' />
+                </div>
+                <span className='category'>
+                    Technology
+                </span>
+                <p>
+                    The Impact of Technology on the Workplace: How Technology is Changing
+                </p>
+                <span className="date">
+                    <CgCalendarDates />
+                    August 20, 2022
+                </span>
+            </div>
+            <div className="read-more">
+                <Link href='/'>
+                    Read more
+                    <FaLongArrowAltRight />
+                </Link>
+            </div>
+        </section>
+    )
+}
+
+export default PostCards
