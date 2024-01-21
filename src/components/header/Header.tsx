6804a4989cade7a57ca00c07c43d926a5fd7e004
AzarAhmadov <ehmedovh111@gmail.com>
2024-01-21 11:50:40 +0400

    Added new state for mobile bar

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,24 +1,58 @@
 'use client'
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import { navLinks } from '../constants/constants';
 import Links from './links/Links';
+import { FaBars } from "react-icons/fa6";
 
 const Header: React.FC = () => {
     const session = true
     const isAdmin = true
 
+    const [mobile, setMobile] = useState(false)
+
+    const openMenu = () => {
+        setMobile(!mobile)
+    }
+
     return (
-        <header id='header'>
-            <div className="row">
-                <div className="logo">
-                    AzarDev
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z" /></svg>
+        <>
+            <header id='header'>
+                <div className="row">
+                    <div className="logo">
+                        <Link href='/'> AzarDev</Link>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z" /></svg>
+                    </div>
+                    <ul className='nav'>
+                        {
+                            navLinks.map((el, idx) => (
+                                <Links title={el.title} path={el.path} key={idx} />
+                            ))
+                        }
+                    </ul>
+                    <div>
+                        {session ? (
+                            <>
+                                {isAdmin && <Links title="Admin" path="/admin" key={-1} />}
+                                <Link href='/login' className='login'> Logout </Link>
+                            </>
+                        ) : (
+                            <Links title="Login" path="/login" key={-1} />
+                        )}
+                    </div>
                 </div>
+                <div onClick={openMenu} className="bar">
+                    <FaBars />
+                </div>
+            </header>
+
+            <div className={`mobile-header ${mobile ? "active" : ''}`}>
                 <ul>
                     {
                         navLinks.map((el, idx) => (
-                            <Links title={el.title} path={el.path} key={idx} />
+                            <div onClick={openMenu}>
+                                <Links title={el.title} path={el.path} key={idx} />
+                            </div>
                         ))
                     }
                 </ul>
@@ -33,7 +67,7 @@ const Header: React.FC = () => {
                     )}
                 </div>
             </div>
-        </header>
+        </>
     );
 };
 
