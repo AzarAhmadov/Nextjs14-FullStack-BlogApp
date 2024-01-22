32a7b0e7d4b9328172bb38bec62aaeb6687d06f7
AzarAhmadov <ehmedovh111@gmail.com>
2024-01-22 23:34:22 +0400

    design wos changed

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,18 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const NotFound = () => {
     return (
-        <div>
-            Not Found
-        </div>
+        <main>
+            <section id='not-found'>
+                <div className="img-area">
+                    <Image alt='page not found' src='/images/not-found.png' fill />
+                </div>
+                <span> page not found  </span>
+                <Link href='/'> Go back </Link>
+            </section>
+        </main>
     )
 }
 
