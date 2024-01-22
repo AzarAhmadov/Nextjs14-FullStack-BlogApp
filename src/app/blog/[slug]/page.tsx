af8c6a54faf5122b74bb0ac6cb9e44a6d43d3f38
AzarAhmadov <ehmedovh111@gmail.com>
2024-01-22 15:11:28 +0400

    fetching data from MongoDB

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,34 +1,34 @@
-import Image from 'next/image'
-import React from 'react'
-import { CgCalendarDates } from "react-icons/cg";
+import PostUser from '@/components/postUser/PostUser';
+import { getPost } from '@/lib/data';
+import React from 'react';
+interface PageDetailProps {
+    params: {
+        slug: string;
+    };
+}
 
-const page: React.FC = () => {
+const Page: React.FC<PageDetailProps> = async ({ params }) => {
+
+    const { slug } = params;
+
+    const data = await getPost(slug);
+
     return (
         <main>
             <section id='blog-detail'>
                 <div className="row">
                     <div className="left">
-                        <Image fill src='/images/post.png' alt='blog_detail' />
+                        <img src={data?.img} alt='blog_detail' />
                     </div>
                     <div className="right">
-                        <h4> Lorem ipsum dolor sit amet consectetur. </h4>
-                        <div className="author">
-                            <div>
-                                <Image width={35} height={35} src='/images/man.png' alt='man' />
-                                <span>
-                                    Ernie Smith
-                                </span>
-                            </div>
-                            <span className="date">
-                                <CgCalendarDates />  August 20, 2024
-                            </span>
-                        </div>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, ea? Deleniti distinctio in vitae architecto expedita minus, eveniet consequatur? Quia provident inventore similique dicta ea blanditiis suscipit. Corrupti alias sint reprehenderit aliquam eveniet tempore doloremque quod possimus praesentium, temporibus molestias ipsum inventore ad ullam ipsam! Saepe qui maxime incidunt nulla laborum aperiam aspernatur doloremque voluptatum optio, debitis aut deleniti repellat ut minus. Magnam quod omnis magni aut esse asperiores officia quia recusandae ex, consequuntur possimus reiciendis eius laborum rerum perferendis minima laboriosam est adipisci unde excepturi sint libero sed hic. Ut in illum totam itaque consequuntur assumenda sunt illo? Perspiciatis?</p>
+                        <h4> {data.title} </h4>
+                        <PostUser data={data} />
+                        <p>{data.desc}</p>
                     </div>
                 </div>
             </section>
         </main>
-    )
-}
+    );
+};
 
-export default page
+export default Page;
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,14 +1,21 @@
+
 import PostCards from '@/components/postCards/PostCards'
+import { getPosts } from '@/lib/data'
+import { PostProps } from '@/types/types'
 import React from 'react'
 
-const page: React.FC = () => {
+const page: React.FC = async () => {
+
+    const posts = await getPosts()
+
+    console.log(posts)
+
     return (
         <main>
             <div className="post-row">
-                <PostCards />
-                <PostCards />
-                <PostCards />
-                <PostCards />
+                {posts.map((post: PostProps) => (
+                    <PostCards post={post} key={post.id} />
+                ))}
             </div>
         </main>
     )
