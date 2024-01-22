bf68a7de2fbf7bc3a2ef064738e6888662350c6a
AzarAhmadov <ehmedovh111@gmail.com>
2024-01-23 00:28:28 +0400

    .

---
+++ src/app/loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loading = () => {
+    return (
+        <section id='spinner'>
+            <img width={135} height={135} src='images/spinner.gif' alt="spinner" />
+        </section>
+    )
+}
+
+export default loading
