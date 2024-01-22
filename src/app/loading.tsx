1e031134963d15ad132cdf111c20a945c92f7fc1
AzarAhmadov <ehmedovh111@gmail.com>
2024-01-23 00:21:13 +0400

    .

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const loading = () => {
     return (
-        <section id='spinner'>
-            <img width={135} height={135} src='images/spinner.gif' alt="spinner" />
-        </section>
+        // <section id='spinner'>
+        //     <img width={135} height={135} src='images/spinner.gif' alt="spinner" />
+        // </section>
+        <div>
+            loading
+        </div>
     )
 }
 
