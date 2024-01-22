bf54ba5f43f6a0d5b62302cfd832ec5096a1dba7
AzarAhmadov <ehmedovh111@gmail.com>
2024-01-22 23:51:24 +0400

    Created register page

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,10 +1,26 @@
+import Image from 'next/image'
 import React from 'react'
 
 const page: React.FC = () => {
     return (
-        <div>
-
-        </div>
+        <main>
+            <section id='register'>
+                <div className="row">
+                    <div className="right">
+                        <form className='form' action="">
+                            <input type="text" placeholder='username' name='username' />
+                            <input type="email" placeholder='email' name='email' />
+                            <input type="password" placeholder='password' name='password' />
+                            <input type="password" placeholder='password again' name='passwordRepad' />
+                            <button> Register </button>
+                        </form>
+                    </div>
+                    <div className="left">
+                        <Image fill src='/images/register.png' alt='register' />
+                    </div>
+                </div>
+            </section>
+        </main>
     )
 }
 
