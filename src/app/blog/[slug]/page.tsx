3ceee845fc5482e608ae933ae821e6190cf10f6a
AzarAhmadov <ehmedovh111@gmail.com>
2024-01-23 02:08:57 +0400

    deleted main tag

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -24,20 +24,18 @@ const Page: React.FC<PageDetailProps> = async ({ params }) => {
     const data = await getPost(slug);
 
     return (
-        <main>
-            <section id='blog-detail'>
-                <div className="row">
-                    <div className="left">
-                        <img src={data?.img} alt='blog_detail' />
-                    </div>
-                    <div className="right">
-                        <h4> {data.title} </h4>
-                        <PostUser data={data} />
-                        <p>{data.desc}</p>
-                    </div>
+        <section id='blog-detail'>
+            <div className="row">
+                <div className="left">
+                    <img src={data?.img} alt='blog_detail' />
                 </div>
-            </section>
-        </main>
+                <div className="right">
+                    <h4> {data.title} </h4>
+                    <PostUser data={data} />
+                    <p>{data.desc}</p>
+                </div>
+            </div>
+        </section>
     );
 };
 
