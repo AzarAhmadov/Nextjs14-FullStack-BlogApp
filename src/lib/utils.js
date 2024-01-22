1804e2d1e124b7f71066d39194c95869dd942fc2
AzarAhmadov <ehmedovh111@gmail.com>
2024-01-22 15:54:06 +0400

    .

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,18 +1,20 @@
-const MONGO_URI = process.env.NEXT_PUBLIC_MONGO;
+  import mongoose from "mongoose"
 
-export const connectToDb = async () => {
-  try {
-    if (connection.isConnected) {
-      console.log("Using existing connection");
-      return;
+  const connection = {};
+
+  export const connectToDb = async () => {
+    try {
+      if (connection.isConnected) {
+        console.log("Using existing connection");
+        return;
+      }
+      const db = await mongoose.connect(process.env.NEXT_PUBLIC_MONGO, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      connection.isConnected = db.connections[0].readyState;
+    } catch (error) {
+      console.log(error);
+      throw new Error(error);
     }
-    const db = await mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    connection.isConnected = db.connections[0].readyState;
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-    throw new Error(error);
-  }
-};
+  };
