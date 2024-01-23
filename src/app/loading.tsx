0e63e5f4ff3e76948b2af3b13923b2fbff7f10d3
AzarAhmadov <ehmedovh111@gmail.com>
2024-01-23 16:03:12 +0400

    .

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,10 @@
+import Image from 'next/image'
 import React from 'react'
 
 const loading = () => {
     return (
         <section id='spinner'>
-            <img width={135} height={135} src='images/spinner.gif' alt="spinner" />
+            <Image priority={false} width={135} height={135} src='/images/spinner.gif' alt="spinner" />
         </section>
     )
 }
