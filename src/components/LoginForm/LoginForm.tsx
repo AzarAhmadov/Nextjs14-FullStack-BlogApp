feab0b8a86b85c902cf35dd24697c7a2bb6413a3
AzarAhmadov <ehmedovh111@gmail.com>
2024-01-24 23:29:34 +0400

    Created a Login component

---
+++ src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,31 @@
+'use client'
+import React from 'react'
+import { GithubLogin, login } from '@/lib/action';
+import { FaGithub } from "react-icons/fa";
+import { useFormState } from 'react-dom';
+import { FormProps } from '../RegisterForm/RegisterForm';
+import Link from 'next/link';
+
+const LoginForm: React.FC = () => {
+
+    const [state, formAction] = useFormState<FormProps>(login, undefined);
+
+    return (
+        <section id='login-form'>
+            <form className='form' action={formAction}>
+                <input required type="text" name="username" placeholder='Username' />
+                <input required type="password" name="password" placeholder='Password' />
+                <button> Login with Credentials </button>
+            </form>
+            <form action={GithubLogin}>
+                <button className='login-github'> Login with github <FaGithub /> </button>
+            </form>
+            {state?.error && <div style={{ marginBottom: '13px', marginTop: '13px' }} className='error'> {state.error}  </div>}
+            <Link href='/register'>
+                Don't have an account ? <b>Register</b>
+            </Link>
+        </section>
+    )
+}
+
+export default LoginForm
