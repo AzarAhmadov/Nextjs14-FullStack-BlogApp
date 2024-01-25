44dbe8adc86adc0fbc055f2c9e3522eb2710908e
AzarAhmadov <ehmedovh111@gmail.com>
2024-01-25 16:10:48 +0400

    Uptadet

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,25 +1,15 @@
-import Image from 'next/image'
+import RegisterForm from '@/components/RegisterForm/RegisterForm'
 import React from 'react'
+
+export const metadata = {
+    title: 'Register Page',
+    description: 'Register description'
+}
 
 const page: React.FC = () => {
     return (
         <main>
-            <section id='register'>
-                <div className="row">
-                    <div className="right">
-                        <form className='form'>
-                            <input type="text" placeholder='username' name='username' />
-                            <input type="email" placeholder='email' name='email' />
-                            <input type="password" placeholder='password' name='password' />
-                            <input type="password" placeholder='password again' name='passwordRepad' />
-                            <button> Register </button>
-                        </form>
-                    </div>
-                    <div className="left">
-                        <Image fill src='/images/register.png' alt='register' />
-                    </div>
-                </div>
-            </section>
+            <RegisterForm />
         </main>
     )
 }
